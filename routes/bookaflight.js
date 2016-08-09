6a66f1458c909d0e9dc5bfd1f32a4c4a85aca971
Machiraju Venkata Naga Nithesh <Machiraju Venkata Naga Nithesh>
2016-08-09 16:26:53 +0530

    fc

--- routes/bookaflight.js
+++ routes/bookaflight.js
@@ -10,7 +10,7 @@ var db = new Store("data",{pretty:true});
 	router.post('/', function(req, res, next) {
 	var payload = req.body
 	var obj = db.getSync("book-a-flight");
-	
+	console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
@@ -20,18 +20,30 @@ var db = new Store("data",{pretty:true});
 
 	var result;
 	  obj.flight.map(function(flight){
+		  console.log(flight);
     		if(flight.id === payload.id){
     			flight.bookingId = 'KL'+text;
     			flight.bookedclass = payload.category;
 				flight.adult = payload.adult;
 				flight.child = payload.child;
-				flight.dateofjourney =  payload.dateofjourney
+				flight.dateofjourney =  payload.dateofjourney;
+				var passengerdetails = {};
+				passengerdetails.name = payload.passengerdetails.name;
+				passengerdetails.email = payload.passengerdetails.email;
+				passengerdetails.age = payload.passengerdetails.age;
+				passengerdetails.address = payload.passengerdetails.address;
+				passengerdetails.url = payload.passengerdetails.url;
+				flight.passengerdetails = passengerdetails;
     		    result = flight;
     		}
     	});
       
 	  res.send(result);
 });
+	
+
+	7207155393
+	
 	
 	
 module.exports = router;
