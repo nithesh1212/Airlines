cbb73af64377dac56ed4f94473ec15c764f1a88c
Venkata Naga Nithesh Machiraju -X (vemachir - TECH MAHINDRA LIM at Cisco) <vemachir@partnet.cisco.com>
2018-02-14 15:50:27 +0530

    added another space

--- routes/airport.js
+++ routes/airport.js
@@ -11,11 +11,11 @@ var options = {
 		uri: 'https://api.ciscospark.com/v1/memberships',
 		headers: {
 				'Content-Type': 'application/json',
-				'Authorization' : 'Bearer OTI1ZWMwNjQtOTBlOS00NGUzLWFiYWMtYjI1ZWFhNzZiZWY0NGVjNTQyMmUtNjFi'
+				'Authorization' : 'Bearer MDM1NDBmNDYtYWE2Yi00OWJiLTg2ZmEtM2JjM2JjNzBkMjFhMjExZWQ5Y2ItMWQ3'
 		},
 		body: {
-				"roomId": "Y2lzY29zcGFyazovL3VzL1JPT00vZWExMzA1NzAtMTE2ZS0xMWU4LTlhYjQtMDczZmE4Y2YxOGU4",
-				"personEmail": "vmasakat@cisco.com",
+				"roomId": "Y2lzY29zcGFyazovL3VzL1JPT00vODgwNjBjYTAtMTE2Zi0xMWU4LTg1MzUtZTMzYzc2NWY0YTVl",
+				"personEmail": "vemachir@cisco.com",
 				"isModerator": false
 		},
 		json: true // Automatically stringifies the body to JSON
