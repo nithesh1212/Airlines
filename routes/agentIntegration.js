2230f90491997a1a9c964c5871ab32305c261158
= <vemachir@partnet.cisco.com>
2018-03-26 15:44:39 +0530

    added post

--- routes/agentIntegration.js
+++ routes/agentIntegration.js
@@ -4,11 +4,16 @@ var Store = require("jfs");
 var rp = require('request-promise');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
+	
+	
+	var nodePath = req.body.nodePath;
+	
+	
 	
 var options = {
 		method: 'GET',
-		uri: 'http://alfcms-app-stg-01:8080/alfresco/service/dcl/permissions/view?nodeId=workspace://SpacesStore/eb4ee8e6-020e-4383-b3f1-2554ba35d4bc',
+		uri: 'http://alfcms-app-stg-01:8080/alfresco/service/dcl/permissions/view?'+nodePath,
 		headers: {
 				
 				'Authorization' : 'Basic YWxmY21hZG06YWxmY21hZG1Ab2N0MTQ='
@@ -16,14 +21,16 @@ var options = {
 		
 		json: true // Automatically stringifies the body to JSON
 	};
+	
+
  
 	rp(options)
     .then(function (parsedBody) {
        res.send(parsedBody);
     })
     .catch(function (err) {
-       //res.send("Thanks for talking to me !!! It seems you already part of Doc Central Support spark group hope someone can help you there.");
 	  res.send(err);
+	 
     });
 	
 
